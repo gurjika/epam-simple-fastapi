b3ede88f5872363d43a977d44e18e63991e48fbd
Luka <lgurjidze@gmail.com>
2025-03-31 23:10:43 +0400

    update (hardcode) connection string

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgrespass@database-2.chw2ywc68gw1.eu-west-1.rds.amazonaws.com/postgres'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgrespass@database-1.chw2ywc68gw1.eu-west-1.rds.amazonaws.com/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
