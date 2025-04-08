e7015c0888b87f1570581e5697e037c675ffa7cb
Luka <lgurjidze@gmail.com>
2025-04-08 17:34:15 +0400

    update scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-# Update system and install required dependencies
-sudo yum update -y
-sudo yum install -y unzip aws-cli docker git
-
-sudo systemctl start docker
-sudo systemctl enable docker
-
-DOCKER_COMPOSE_VERSION=1.29.2
-sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-rm -f /home/ec2-user/app/project.zip
+rm -rf /home/ec2-user/app/project.zip
--- scripts/unzip_and_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-APP_DIR="/home/ec2-user/app"
-ZIP_FILE="project.zip"
-
-cd $APP_DIR
-unzip -o $ZIP_FILE -d $APP_DIR
