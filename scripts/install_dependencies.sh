817990f6f1e8012db667a52f06a6b336a56e0a71
Luka <lgurjidze@gmail.com>
2025-04-08 17:27:20 +0400

    update appspec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,3 +9,5 @@ sudo systemctl enable docker
 DOCKER_COMPOSE_VERSION=1.29.2
 sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+rm -f /home/ec2-user/app/project.zip
